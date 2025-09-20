70a85e284b64a804bca817452fd8678688cd1bc5
ishratabbas001-ai <ishratabbas001@gmail.com>
2025-09-20 15:01:01 +0500

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [,
-  base: "./"
+  plugins: [
     react()
   ],
   resolve: {
